c4f4c27e2f6b531c044b83b96afad3d85b17736f
borgesop <pborgeson@hanoverschools.org>
2015-03-16 17:00:48 -0400

    distance

---
+++ o if/distance KPR.c
@@ -0,0 +1,16 @@
+// Created on Sun January 25 2015
+
+int main()
+{
+	printf("Hello, World!\n");
+	while (digital(13) == 0)
+	{
+		power_level = (500-analog_et(7) / 2;
+		motor(0,power_level);
+		motor(3,power_level)
+		
+		//msleep(50);
+		printf("power = ,xxxxfind on slidexxxxx power_level);
+	}
+	return 0;
+}
